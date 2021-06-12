beced8c97ffefe5ad8351826336489287a1d32e0
a <a>
2021-06-12 12:54:37 +0700

    upload dockerfile

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+const PORT = process.env.PORT || 8080
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,4 +22,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(PORT);
